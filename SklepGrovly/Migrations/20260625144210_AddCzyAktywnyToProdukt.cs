11729ae4ddd949a18e9dfd9c64928d03a218d630
Konrad Karczewski <konrad228833@gmail.com>
2026-06-25 16:43:37 +0200

    Nowa migracja dodająca do bazy pole CzyAktywny w encji Produky

---
+++ SklepGrovly/Migrations/20260625144210_AddCzyAktywnyToProdukt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SklepGrovly.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCzyAktywnyToProdukt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CzyAktywny",
+                table: "Produkt",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CzyAktywny",
+                table: "Produkt");
+        }
+    }
+}
