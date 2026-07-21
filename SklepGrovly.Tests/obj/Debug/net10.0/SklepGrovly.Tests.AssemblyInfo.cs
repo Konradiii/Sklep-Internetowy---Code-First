a761b7ec3fe4f8935f2d49638560bea80d2e778b
Konrad Karczewski <konrad228833@gmail.com>
2026-07-21 21:11:58 +0200

    Test integracyjny PlaceGuestOrder: znajdz-lub-utworz goscia po emailu

--- SklepGrovly.Tests/obj/Debug/net10.0/SklepGrovly.Tests.AssemblyInfo.cs
+++ SklepGrovly.Tests/obj/Debug/net10.0/SklepGrovly.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SklepGrovly.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2633c146018b72934b21ad065d1897da8a6afc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45bca906c2e33101dbc042d0f4874f424b3f1dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SklepGrovly.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SklepGrovly.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
